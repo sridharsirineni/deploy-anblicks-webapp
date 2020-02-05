dcda8458daf9a919ec98ba214632a4e8fee5d6c4
sridharsirineni <40170121+sridharsirineni@users.noreply.github.com>
2020-02-05 21:09:51 +0530

    Update DownloadWebApp.sh

--- DownloadWebApp.sh
+++ DownloadWebApp.sh
@@ -1,1 +1,1 @@
-wget -O webapp.war  http://192.168.33.101:8081/repository/maven-snapshots/com/anblicks/app/webapp/1.0-SNAPSHOT/webapp-1.0-20200205.133541-3.war
+wget -O webapp.war  http://192.168.33.101:8081/repository/maven-snapshots/com/anblicks/app/webapp/1.0-SNAPSHOT/webapp-1.0-20200205.153757-5.war
