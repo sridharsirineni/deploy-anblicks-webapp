a77541d1df5919982c51e0f180de432955f06eaa
sridhar <sridhar.com>
2020-02-07 08:57:43 +0000

    added latest war file

--- DownloadWebApp.sh
+++ DownloadWebApp.sh
@@ -1,2 +1,3 @@
 rm -rf webapp*
-wget -O webapp.war  http://192.168.33.101:8081/repository/maven-snapshots/com/anblicks/app/webapp/1.0-SNAPSHOT/webapp-1.0-20200206.175453-22.war
+wget -O webapp.war  http://192.168.33.101:8081/repository/maven-snapshots/com/anblicks/app/webapp/1.0-SNAPSHOT/webapp-1.0-20200207.085055-23.war
+
